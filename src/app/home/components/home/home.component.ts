f8a1824c2610db0b5d1153ec809b3dcb5c5a04dd
Pawel Michalak <pawelmichalak98@gmail.com>
2025-06-09 01:20:29 +0200

    rework of homepage

--- src/app/home/components/home/home.component.ts
+++ src/app/home/components/home/home.component.ts
@@ -1,11 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatIconModule } from '@angular/material/icon';
+import { Store } from '@ngrx/store';
+import { authFeature } from '../../../shared/reducers/auth.reducer';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { MatDialog } from '@angular/material/dialog';
+import { LoginDialogComponent } from '../../../shared/components/login-dialog/login-dialog.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatTabsModule,
+    MatIconModule,
+  ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  private store: Store = inject(Store);
+  private matDialog: MatDialog = inject(MatDialog);
+  isLoggedIn$ = this.store.select(authFeature.selectIsLoggedIn);
+  tabs: string[] = [];
+  selected = new FormControl(0);
 
+  addTab(selectAfterAdding: boolean) {
+    this.tabs.push('New');
+
+    if (selectAfterAdding) {
+      this.selected.setValue(this.tabs.length - 1);
+    }
+  }
+
+  removeTab(index: number) {
+    this.tabs.splice(index, 1);
+    this.selected.setValue(index);
+  }
+  onOpenLogin() {
+    this.matDialog.open(LoginDialogComponent);
+  }
 }
